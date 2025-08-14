e6a7a6121a9161df7f7272fcc39fb069f0b8a1f9
Jenna <heo980201@gmail.com>
2025-08-14 19:49:32 +1000

    change dates

--- frontend/src/pages/CalendarEvents.jsx
+++ frontend/src/pages/CalendarEvents.jsx
@@ -4,8 +4,8 @@ import axiosInstance from '../axiosConfig';
   
   function CalendarwithEvents() {
     const events = [
-      { title: 'dueDate', date: '2024-08-15' },
-      { title: 'finalExam', date: '2024-010-27' },
+      { title: 'dueDate', date: '2024-08-17' },
+      { title: 'finalExam', date: '2024-10-27' },
     ];
 
     return (
