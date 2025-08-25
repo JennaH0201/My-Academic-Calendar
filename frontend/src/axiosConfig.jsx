3582abbb254a68aede2a5ca93d8069deda28e5d8
Jenna <heo980201@gmail.com>
2025-08-25 17:43:15 +1000

    test

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', //local
+  baseURL:' ',  //local
   //baseURL: 'http://3.26.165.163:5001', // live
  
   headers: { 'Content-Type': 'application/json' },
